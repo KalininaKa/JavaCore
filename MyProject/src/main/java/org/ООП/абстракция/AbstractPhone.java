ddd85712d993666a8411c0cc2b1ccf45ec42e354
Kalinina Karina <kalinina.karina10@yandex.ru.ru>
2023-08-29 15:12:14 +0300

    add oop

---
+++ MyProject/src/main/java/org/ООП/абстракция/AbstractPhone.java
@@ -0,0 +1,11 @@
+package org.ООП.абстракция;
+
+public abstract class AbstractPhone {
+    private int year;
+
+    public AbstractPhone(int year) {
+        this.year = year;
+    }
+    public abstract void call(int outputNumber);
+    public abstract void ring (int inputNumber);
+}
